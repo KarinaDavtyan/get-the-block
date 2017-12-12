6ce20478114c21c8698b14a3637e2ecbc6385e7f
KarinaDavtyan <k.davtyan27@gmail.com>
2017-12-12 23:42:45 +0100

    feat: implement unsplash api for background img use

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LogIn from './LogIn';
+import cred from './cred';
 
 class App extends Component {
 
-  
+  constructor() {
+		super();
+		this.state = {
+			imgs: null
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api.unsplash.com/photos/random/?client_id=' + cred.APP_ID)
+			.then(res => res.json())
+			.then(data => {
+				this.setState({ imgs: data });
+				console.log(this.state.imgs.urls.raw);
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	}
+
+	renderImage = () => {
+		if (this.state.imgs) {
+			return (
+				<div className="background">
+					<img src={this.state.imgs.urls.raw} />
+				</div>
+			)
+		}
+	}
+
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
           <LogIn />
+					{this.renderImage()}
         </div>
       </MuiThemeProvider>
     );
