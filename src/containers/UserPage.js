0b82451420e4f301b81662dd19ef4fe082fb1277
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-08 22:58:14 +0100

    passing username in the query string

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -2,34 +2,26 @@ import React from 'react';
 
 class UserPage extends React.Component {
 
-  getBlockList () {
-    fetch('http://Karina-MacBookPro.local:3000/get_the_blocks')
+  getBlockList = (data) => {
+    const params = {
+      method: 'GET',
+    }
+    const user = window.location.pathname.slice(1);
+    fetch(`http://Karina-MacBookPro.local:3000/get_the_blocks${user}`, params)
       .then(blocks => blocks.json())
       .then(blocks => {
         console.log(blocks);
       })
   }
 
-  blockTheUser (data) {
-    fetch('http://Karina-MacBookPro.local:3000/block_it',
-      {
-        method: 'POST',
-        body: JSON.stringify(data)
-      })
-  }
 
   render () {
     return (
       <div className="UserPage">
         hey UserPage
-        <button
-          primary
-          onClick={this.getBlockList}
-        />
         <br/>
         <button
-          primary
-          onClick={this.blockTheUser('arolet')}
+          onClick={this.getBlockList}
         />
       </div>
     );
