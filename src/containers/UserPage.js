9bf422d041b7e964e674759be9f1c959e2c0b968
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-09 22:51:30 +0100

    passing user_to_block to th ebackend

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -2,18 +2,38 @@ import React from 'react';
 
 class UserPage extends React.Component {
 
-  getBlockList = (data) => {
+  state = {
+    toBlock: '',
+  }
+
+  getBlockList = () => {
     const params = {
       method: 'GET',
     }
     const user = window.location.pathname.slice(1);
-    fetch(`http://Karina-MacBookPro.local:3000/get_the_blocks${user}`, params)
+    fetch(`http://Karina-MacBookPro.local:3000/get_the_blocks/${user}`, params)
       .then(blocks => blocks.json())
       .then(blocks => {
         console.log(blocks);
       })
   }
 
+  block = (username) => {
+    const params = {
+      method: 'POST',
+    }
+    const user = window.location.pathname.slice(1);
+    console.log(user, username);
+    fetch('http://Karina-MacBookPro.local:3000/block_it/' + user + '/' + username, params)
+  }
+
+  handleChanges = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  validate = () => this.block(this.state.toBlock);
 
   render () {
     return (
@@ -22,6 +42,15 @@ class UserPage extends React.Component {
         <br/>
         <button
           onClick={this.getBlockList}
+        />
+        <button
+          onClick={this.validate}
+        />
+        <input
+          value={this.state.toBlock}
+          onChange={this.handleChanges}
+          id="toBlock"
+          name="toBlock"
         />
       </div>
     );
