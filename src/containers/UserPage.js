e4c68de8dfa7f754d413efb48126b207549e367f
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-11 21:38:39 +0100

    added input to follow user

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class UserPage extends React.Component {
 
   state = {
     user: window.location.pathname.slice(1),
     toBlock: '',
+    toFollow: ''
   }
 
 
@@ -30,13 +32,22 @@ class UserPage extends React.Component {
     fetch('http://Karina-MacBookPro.local:3000/block_it/' + user + '/' + username, params)
   }
 
+  follow = (username) => {
+    const params = {
+      method: 'POST',
+    }
+    const { user } = this.state;
+    fetch('http://Karina-MacBookPro.local:3000/follow_block/' + user + '/' + username, params)
+  }
+
   handleChanges = (e) => {
     this.setState({
       [e.target.name]: e.target.value
     })
   }
 
-  validate = () => this.block(this.state.toBlock);
+  validateToBlock = () => this.block(this.state.toBlock);
+  validateToFollow = () => this.follow(this.state.toFollow);
 
   render () {
     return (
@@ -47,15 +58,29 @@ class UserPage extends React.Component {
           onClick={this.getBlockList}
           label='get block list'
         />
+        <br/>
         <RaisedButton
-          onClick={this.validate}
+          onClick={this.validateToBlock}
           label='block the input user'
         />
-        <input
+        <TextField
           value={this.state.toBlock}
           onChange={this.handleChanges}
           id="toBlock"
           name="toBlock"
+          hintText="toBlock"
+        />
+        <br/>
+        <RaisedButton
+          onClick={this.validateToFollow}
+          label='follow the input user'
+        />
+        <TextField
+          value={this.state.toFollow}
+          onChange={this.handleChanges}
+          id="toFollow"
+          name="toFollow"
+          hintText="toFollow"
         />
       </div>
     );
